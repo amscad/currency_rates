04e87eb5ce83266e78d79e4087bb8a29a5a06f01
Alex Mansfield <alex@inyoni.com>
2013-09-15 11:16:48 +0200

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurrencyRates::Application.config.secret_key_base = 'c7cac763148d91c3c7a537ea67ce87efc334aa4be777c4f57b149d735b1c53e562ba0eca89069a58be8b193bf546029f99494e3595489b2cf62480f7f5794b80'
